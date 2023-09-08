ba6b890aaaade465deb5d0cbaef29b127f33f237
ERINA KATAOKA <nmerinaaay324@gmail.com>
2023-09-08 12:05:08 +0900

    下層ページ最終修正

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,72 @@
-
 // ローディングアニメーション
-const keyName = 'visited';
-const keyValue = true;
-
-const openingAnimation = () => {
-  const openingTL = gsap.timeline();
-  openingTL
-  .fromTo('.js-loading-title',
-    {clipPath:'inset(100% 0 0 0)',scale:1.1,autoAlpha:0},
-    {clipPath:'inset(0% 0 0 0)',scale:1,autoAlpha:1,duration:2,ease:'power4.out',delay:1})
-  .fromTo('.js-loading-img',
-    {y:'100%'},
-    {y:'0%',duration:2,ease:'power4.out',stagger:.1},'-=0.5')
-  .to('.js-loading-title',{autoAlpha:0,duration:.8},'<')
-  .fromTo('.js-loading-title',
-    {autoAlpha:0,scale:0.9},
-    {autoAlpha:1,scale:1,duration:1,ease:'power4.in',color:'#fff'},'-=.5')
-  .fromTo('.js-loading',{autoAlpha:1},{autoAlpha:0,duration:1,ease:'power4.in'},'+=2')
+// クッキー登録
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
-
-window.addEventListener('DOMContentLoaded', () => {
-  if (!sessionStorage.getItem(keyName)) {
-    // 初回訪問時の処理
-    sessionStorage.setItem(keyName, keyValue);
-    openingAnimation();
-  } else {
-    // 2回目以降の訪問時の処理
-    const loadingElement = document.querySelector('.js-loading');
-    if (loadingElement) {
-      loadingElement.style.display = 'none';
-    }
+// クッキーを取得
+function getCookie(name) {
+  const value = "; " + document.cookie;
+  const parts = value.split("; " + name + "=");
+  if (parts.length === 2){
+      return parts.pop().split(";").shift();
+  }else{
+      return "";
   }
+}
+// アニメーション再生
+const loadingAnime = document.querySelector('.js-loading');
+const body = document.body;
+const scrollPosition = window.scrollY;
+function playAnimation() {
+  if (loadingAnime) {
+    body.style.overflow = 'hidden'; // スクロールを禁止
+    const openingTL = gsap.timeline();
+    openingTL
+    .fromTo('.js-loading-title',
+      {clipPath:'inset(100% 0 0 0)',scale:1.1,autoAlpha:0},
+      {clipPath:'inset(0% 0 0 0)',scale:1,autoAlpha:1,duration:2,ease:'power4.out',delay:1})
+    .fromTo('.js-loading-img',
+      {y:'100%'},
+      {y:'0%',duration:2,ease:'power4.out',stagger:.1},'-=0.5')
+    .to('.js-loading-title',{autoAlpha:0,duration:.8},'<')
+    .fromTo('.js-loading-title',
+      {autoAlpha:0,scale:0.9},
+      {autoAlpha:1,scale:1,duration:1,ease:'power4.in',color:'#fff'},'-=.5')
+    .fromTo('.js-loading',{autoAlpha:1},{autoAlpha:0,duration:1,ease:'power4.in'},'+=2')
+    .call(enableScroll); // アニメーション終了時にスクロールを有効にする
+  }
+}
+function enableScroll() {
+  // スクロールを有効にする
+  body.style.overflow = 'auto';
+  // スクロール位置を元に戻す（任意の位置にスクロールさせない場合はこの行を削除できます）
+  window.scrollTo(0, scrollPosition);
+}
+// オープニングアニメーションに関わる要素を非表示
+function hideAnimation() {
+  if (loadingAnime) {
+    gsap.set('.js-loading',{autoAlpha:0})
+  }
+}
+// まず最初に読み込まれる所
+document.addEventListener("DOMContentLoaded", function() {
+  const animationPlayed = getCookie("animationPlayed");
+  if (animationPlayed) {
+    hideAnimation();
+  } else {
+    playAnimation();
+    setCookie("animationPlayed", "true", 1);
+  }
+});
+
+
+// ページ遷移
+window.addEventListener('load', function() {
+  const body = document.querySelector('body');
+  body.classList.add('is-active');
 });
 
 
